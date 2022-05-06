0c4e9cc79ff577211b836d360f24fafb9e7b0390
Leelakalyan <99253536+Leelakalyan@users.noreply.github.com>
2022-05-06 21:04:24 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+c={1:['kalyan',6785383325,50000,'tuni'],2:['ramesh',8706543267,60000,'kakinada'],3:['saraswathi',9704545778,45000,'rajamundry']}
+x=int(input('enter the employee no:'))
+print('name of the employee:',c[x][0])
+print('emppno:',c[x][1])
+print('empsalary:',c[x][2])
+print('city:',c[x][3])
+
+output:
+enter the employee no:1
+name of the employee: kalyan
+emppno: 6785383325
+empsalary: 50000
+city: tuni
